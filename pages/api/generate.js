86eb3c5a3dd19e34590681464393c5ed7efd3304
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2023-05-21 23:36:51 +0530

    Update generate.js should work

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
-const OPENAI_API_KEY= "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"
-
 const configuration = new Configuration({
-  apiKey: OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
