939e3c0c83f6fcf38fabdcbfc3c4934861e3231f
Anurag Singh <90863914+anurag3811@users.noreply.github.com>
2023-05-22 23:37:41 +0530

    last some changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,13 @@
 import { Configuration, OpenAIApi } from "openai";
 
+const part1 = "sk-rjqLfbflyMdAsj4KKPVST3B"
+
+const part2 = "lbkFJWVgKIcH5jCwIo2R5lVPf" 
+
+const KEY = part1+part2
+
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: KEY,
 });
 const openai = new OpenAIApi(configuration);
 
